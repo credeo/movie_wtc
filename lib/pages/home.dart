ed65e722c9e8814596e5a0de83b8c68c255085ad
macbookpro <macbookpro@macbookpros-MacBook-Pro.local>
2022-08-03 10:08:53 +0200

    s

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
                     });
                   },
                   controller: _controller,
-                  itemCount: 3,
+                  itemCount: models.length,
                   itemBuilder: (BuildContext context, int index) {
                     currentIndex = index;
                     return Stack(
