8e64b8777ecb69e4f51bb782dcf2c84c848c119f
filipraj <filipraj08@gmail.com>
2022-08-09 13:12:06 +0200

    implement buttons in coming soon

--- lib/providers/coming_soon_provider.dart
+++ lib/providers/coming_soon_provider.dart
@@ -2,10 +2,12 @@ import 'package:flutter/foundation.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:movie_wtc/models/movie.dart';
 import 'package:movie_wtc/services/movie_service.dart';
+import 'package:movie_wtc/services/user_service.dart';
 
 enum HomeState { loading, ready }
 
 class ComingSoonProvider extends ChangeNotifier {
+  final _userService = KiwiContainer().resolve<UserService>();
   final _movieService = KiwiContainer().resolve<MovieService>();
   HomeState _state = HomeState.loading;
   bool _isDisposed = false;
@@ -27,5 +29,19 @@ class ComingSoonProvider extends ChangeNotifier {
     await _movieService.fetchComingSoonMovies();
     _state = HomeState.ready;
     if (!_isDisposed) notifyListeners();
+  }
+
+  void addMovieToMyList(Movie movie) {
+    _userService.addToMyList(movie);
+    notifyListeners();
+  }
+
+  void removeMovieFromMyList(Movie movie) {
+    _userService.removeFromMyList(movie);
+    notifyListeners();
+  }
+
+  bool isMovieInMyList(Movie movie) {
+    return _userService.isMovieInMyList(movie);
   }
 }
