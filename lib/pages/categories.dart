7799731df1c22d259be85a554d4174637647fa3d
Klasa <janko36@outlook.com>
2022-08-12 08:56:05 +0200

    categories page

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:movie_wtc/extensions/custom_colors.dart';
 import 'package:movie_wtc/extensions/custom_text_styles.dart';
 import 'package:movie_wtc/models/movie.dart';
+import 'package:movie_wtc/pages/movie_details.dart';
+import 'package:movie_wtc/providers/categories_provider.dart';
 import 'package:movie_wtc/widgets/custom_app_bar.dart';
+import 'package:provider/provider.dart';
 
 class CategoriesPage extends StatelessWidget {
   static const pageName = 'categories';
@@ -11,25 +15,39 @@ class CategoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(
-        hasBackButton: true,
-        hasSearchButton: true,
+    return ChangeNotifierProvider<CategoriesProvider>(
+      create: (context) => CategoriesProvider(),
+      child: Consumer<CategoriesProvider>(
+        builder: (context, categoriesProvider, child) {
+          return Scaffold(
+            backgroundColor: CustomColors.of(context).background,
+            appBar: const CustomAppBar(
+              hasBackButton: true,
+              hasSearchButton: true,
+            ),
+            body: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: categoriesProvider.map.keys.length,
+                itemBuilder: (context, index) {
+                  return getCategorySection(
+                      context: context,
+                      categoryTitle:
+                          categoriesProvider.map.keys.elementAt(index),
+                      categoriesProvider: categoriesProvider);
+                }),
+          );
+        },
       ),
-      body: ListView.builder(
-          padding: EdgeInsets.zero,
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return getCategorySection(
-                context: context, categoryTitle: 'Romance');
-          }),
     );
   }
 
-  Widget getCategorySection(
-      {required BuildContext context, required String categoryTitle}) {
+  Widget getCategorySection({
+    required BuildContext context,
+    required String categoryTitle,
+    required CategoriesProvider categoriesProvider,
+  }) {
     return Container(
-      height: 310,
+      height: 301,
       child: Column(
         children: [
           Padding(
@@ -42,8 +60,10 @@ class CategoriesPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  categoryTitle,
+                  '${categoryTitle[0].toUpperCase()}${categoryTitle.substring(1)}',
                   style: CustomTextStyles.of(context).semiBold18,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
                 ),
                 Text(
                   'See More',
@@ -61,10 +81,14 @@ class CategoriesPage extends StatelessWidget {
               child: ListView.builder(
                 padding: EdgeInsets.zero,
                 scrollDirection: Axis.horizontal,
-                itemCount: 20,
                 itemBuilder: (context, index) {
-                  return getOneItem(context: context, movie: Movie.test(0));
+                  return getOneItem(
+                    context: context,
+                    movie:
+                        categoriesProvider.map[categoryTitle]!.elementAt(index),
+                  );
                 },
+                itemCount: categoriesProvider.map[categoryTitle]!.length,
               ),
             ),
           )
@@ -74,33 +98,45 @@ class CategoriesPage extends StatelessWidget {
   }
 
   Widget getOneItem({required BuildContext context, required Movie movie}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: SizedBox(
-            height: 170,
-            width: 134,
-            child: Image.asset(
-              movie.coverImage,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        context.goNamed(MovieDetails.pageName, params: {'id': movie.id});
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: SizedBox(
+              height: 170,
+              width: 134,
+              child: Image.asset(
+                movie.coverImage,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 8),
-        Text(
-          movie.title,
-          style: CustomTextStyles.of(context).semiBold14,
-        ),
-        SizedBox(height: 8),
-        Text(
-          movie.details,
-          style: CustomTextStyles.of(context).regular12.apply(
-                color: CustomColors.of(context).inactive,
-              ),
-        ),
-      ]),
+          SizedBox(height: 8),
+          SizedBox(
+            width: 134,
+            child: Text(
+              movie.title,
+              style: CustomTextStyles.of(context).semiBold14,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          SizedBox(height: 8),
+          SizedBox(
+            width: 134,
+            child: Text(
+              movie.details,
+              style: CustomTextStyles.of(context).regular12.apply(
+                    color: CustomColors.of(context).inactive,
+                  ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
---
+++ lib/providers/categories_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:kiwi/kiwi.dart';
+import 'package:movie_wtc/models/movie.dart';
+import 'package:movie_wtc/services/movie_service.dart';
+
+class CategoriesProvider extends ChangeNotifier {
+  final Map<String, List<Movie>> _map = <String, List<Movie>>{};
+  final List<Movie> _movies =
+      KiwiContainer().resolve<MovieService>().suggestedMovies;
+
+  Map<String, List<Movie>> get map => _map;
+
+  CategoriesProvider() {
+    for (Movie m in _movies) {
+      for (int i = 0; i < m.genres.length; i++) {
+        _map.putIfAbsent(m.genres[i].value, () => []);
+        _map[m.genres[i].value]!.add(m);
+        print(_map[m.genres[i].value]!.length);
+      }
+    }
+  }
+}
