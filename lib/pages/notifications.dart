d56b53400af0e6473b1f2f3f97bc10ac82f91ffd
Klasa <91409403+JankoTufegdzic@users.noreply.github.com>
2022-08-17 09:45:00 +0200

    work on notifications

--- lib/pages/notifications.dart
+++ lib/pages/notifications.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:movie_wtc/extensions/custom_colors.dart';
+import 'package:movie_wtc/extensions/custom_text_styles.dart';
+import 'package:movie_wtc/pages/movie_details.dart';
 import 'package:movie_wtc/providers/notifications_provider.dart';
 import 'package:movie_wtc/widgets/custom_app_bar.dart';
 import 'package:provider/provider.dart';
@@ -17,11 +20,53 @@ class NotificationsPage extends StatelessWidget {
         builder: (context, notificationsProvider, child) {
           return Scaffold(
             backgroundColor: CustomColors.of(context).background,
-            appBar: CustomAppBar(hasBackButton: true),
+            appBar: const CustomAppBar(hasBackButton: true),
             body: ListView.builder(
               itemCount: notificationsProvider.notifyMeList.length,
               itemBuilder: (context, index) {
-                return Text('janko');
+                final movie = notificationsProvider.notifyMeList[index];
+                return GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () {
+                    context.pushNamed(MovieDetails.pageName,
+                        params: {'id': movie.id});
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 12),
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(6),
+                            child: SizedBox(
+                              width: 100,
+                              child: Image.asset(movie.coverImage,
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            movie.title,
+                            style: CustomTextStyles.of(context).semiBold18,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          ),
+                        ),
+                        Text(
+                          'in ${movie.releaseDate.difference(DateTime.now()).inDays.toString()} days',
+                          style: CustomTextStyles.of(context).medium12.apply(
+                                color: CustomColors.of(context).primary,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
               },
             ),
           );
